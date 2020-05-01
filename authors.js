6f70327f5690b64a2e1948228e0276008a140296
Alexey Agapov <agapov.one@gmail.com>
2020-05-01 13:03:15 +0500

    Add one more author

--- authors.js
+++ authors.js
@@ -3,5 +3,6 @@ import authorId from './helpers/author-id';
 export default authorId([
   // post: false
   // { username: '', start: '27 Apr 2020', first: '', post: false },
-  { username: 'ikimruslan', start: '27 Apr 2020', first: '1254632185081892865' },
+  { post: false, username: 'ikimruslan', start: '27 Apr 2020', first: '1254632185081892865' },
+  { username: 'xd720p', start: '20 Apr 2020', first: '1252111772330401794' },
 ]);
