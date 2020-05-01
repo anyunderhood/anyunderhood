8699134a6fcfec304d896fc3217b3f9f50a6bf63
Alexey Agapov <agapov.one@gmail.com>
2020-05-01 13:40:41 +0500

    Add more authors

--- authors.js
+++ authors.js
@@ -4,5 +4,6 @@ export default authorId([
   // post: false
   // { username: '', start: '27 Apr 2020', first: '', post: false },
   { username: 'ikimruslan', start: '27 Apr 2020', first: '1254632185081892865' },
-  // { username: 'xd720p', start: '20 Apr 2020', first: '1252111772330401794' },
+  { username: 'xd720p', start: '20 Apr 2020', first: '1252111772330401794' },
+  { username: 'atimca', start: '13 Apr 2020', first: '1249600964849537026' },
 ]);
