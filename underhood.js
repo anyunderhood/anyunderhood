cc354af6b805e5010f8cb5552e3b9abbb587dec7
Alexey Agapov <agapov.one@gmail.com>
2020-05-02 00:48:32 +0500

    Update underhood.js file

--- underhood.js
+++ underhood.js
@@ -1,30 +1,25 @@
 const name = "mobileunderhood"
+const site = "mobileunderhood.ru"
 
 const description = "Коллективный твиттер-аккаунт для мобильных разработчиков с новым автором каждую неделю"
-
-const email = "agapov.onede@gmail.com"
-const authorName = ["Alex Agapov", "Pavel Trofimov"] // TODO: AUTHORS
-const author = email + " (" + authorName.join(', ') + ")"
 
 module.exports = {
   "underhood": name,
   "underhoodDesc": description,
-  "underhoodSite": name + ".ru",
-  "githubUser": "WantSomeTea",
-  "githubRepo": "anyunderhood",
+  "underhoodSite": site,
+  "githubUser": "etolstoy",
+  "githubRepo": "mobileunderhood",
   "curatorEmail": "igrekde@gmail.com",
   "curatorTwitter": "igrekde",
   "creatorTwitter": "igrekde",
-  "gauges": "",
+  "creatorCreds": "igrekde@gmail.com (Egor Tolstoy)",
   "googleAnalytics": "",
   "yandexMetric": "",
-  "creatorCreds": author,
-  "underhoodVersion": "0.1.2",
   "site": {
     "title": "Сайт @" + name,
     "description": description,
-    "feed_url": "https://" + name + ".ru/rss.xml",
-    "site_url": "https://" + name + ".ru/",
+    "feed_url": "https://" + site + "/rss.xml",
+    "site_url": "https://" + site + "/",
     "managingEditor": author,
     "webMaster": author,
     "copyright": "MIT",
