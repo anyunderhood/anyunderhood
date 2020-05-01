31eee2a69c74eb9ce93ceaec646c920b4497b57b
Alexey Agapov <agapov.one@gmail.com>
2020-05-02 01:00:27 +0500

    Fix author presence in underhood.js

--- underhood.js
+++ underhood.js
@@ -3,6 +3,8 @@ const site = "mobileunderhood.ru"
 
 const description = "Коллективный твиттер-аккаунт для мобильных разработчиков с новым автором каждую неделю"
 
+const author = "igrekde@gmail.com (Egor Tolstoy)"
+
 module.exports = {
   "underhood": name,
   "underhoodDesc": description,
@@ -12,7 +14,7 @@ module.exports = {
   "curatorEmail": "igrekde@gmail.com",
   "curatorTwitter": "igrekde",
   "creatorTwitter": "igrekde",
-  "creatorCreds": "igrekde@gmail.com (Egor Tolstoy)",
+  "creatorCreds": author,
   "googleAnalytics": "",
   "yandexMetric": "",
   "site": {
