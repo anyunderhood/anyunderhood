f2cbf269faac7dd8c10fc538fc2a926c3fb03eff
Alexey Agapov <agapov.one@gmail.com>
2020-05-02 16:59:52 +0500

    Rename all -> list

--- scripts/update.js
+++ scripts/update.js
@@ -1,6 +1,8 @@
 import authors from "../authors";
 import update from "./update-one";
 
-// import updateAll from "./update-all";
+/// Update authors array if you want to.
+// import updateList from "./update-list";
+// updateList(authors);
 
 update(authors[0]);
