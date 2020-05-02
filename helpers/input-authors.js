27ed6bb53d7e2c3d9dbfabd206ce21b57e141d7e
Alexey Agapov <agapov.one@gmail.com>
2020-05-02 18:59:43 +0500

    Add a merger in media for absence of banner/image

--- helpers/input-authors.js
+++ helpers/input-authors.js
@@ -1,11 +1,15 @@
-import { map, merge } from 'ramda';
-import authors from './rendered-authors';
-import getAuthorArea from './get-author-area';
+import { map, merge } from 'ramda'
+import authors from './rendered-authors'
+import getAuthorArea from './get-author-area'
 
-const saturate = author => merge(author, {
-  info: getAuthorArea(author.username, 'info') || {},
-  tweets: getAuthorArea(author.username, 'tweets').tweets || [],
-  media: getAuthorArea(author.username, 'media') || {}
-});
+const saturate = (author) =>
+  merge(author, {
+    info: getAuthorArea(author.username, 'info') || {},
+    tweets: getAuthorArea(author.username, 'tweets').tweets || [],
+    media: merge(
+      { image: '', banner: '' },
+      getAuthorArea(author.username, 'media')
+    ),
+  })
 
-export default map(saturate, authors);
+export default map(saturate, authors)
