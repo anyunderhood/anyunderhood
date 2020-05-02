02a59e0dbe09c3a1c9500b1e029a981486b15b2e
Alexey Agapov <agapov.one@gmail.com>
2020-05-02 17:19:20 +0500

    Save dump-old

---
+++ dump-old/index.js
@@ -0,0 +1,12 @@
+import { map, merge } from 'ramda';
+import authors from '../authors';
+import getAuthorArea from '../helpers/get-author-area';
+
+const saturate = author => merge(author, {
+  info: getAuthorArea(author.authorId, 'info') || {},
+  followers: getAuthorArea(author.authorId, 'followers').followers || [],
+  tweets: getAuthorArea(author.authorId, 'tweets').tweets || [],
+  media: getAuthorArea(author.authorId, 'media') || {},
+});
+
+export default map(saturate, authors);
