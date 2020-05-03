d47f76075e65edd334487912a7bf43bfeafa7bc7
Alexey Agapov <agapov.one@gmail.com>
2020-05-03 20:37:03 +0500

    Remove time-zone-offset from authors

--- scripts/migrator/migrate-info.js
+++ scripts/migrator/migrate-info.js
@@ -4,10 +4,6 @@ import { merge } from "ramda";
 fixer('info', 'dump-old', 'dump-old',
   (content) =>
     merge(
-      content,
-      {
-        time_zone_offset: 0,
-        geometry: { lat: 0.0, lng: 0.0 }
-      }
+      content
     )
 )
