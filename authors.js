d9b56d1877475bac73eddb8af891e28ebe0043bd
p.y.trofimov <p.y.trofimov@gmail.com>
2020-05-26 14:34:33 +0300

    authorsjs template

--- authors.js
+++ authors.js
@@ -1,10 +1,9 @@
+import authorId from './helpers/author-id';
 
-        import authorId from './helpers/author-id';
-        export default authorId([
-        {
-            username: autor_username,
-            first: 123,
-            post: false
-        },
-        ]);
-        
+export default authorId([
+  // {
+  //   username: '' // twitter name of author. if author has no twitter - use underhood name
+  //   first: '',  # id of first tweet
+  //   post: false # Include only for author that hasn't finished his week yet. Will display link to twitter instead of archive
+  // },
+]);
